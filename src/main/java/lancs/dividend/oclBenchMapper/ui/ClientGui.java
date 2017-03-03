a0ff0580db465745061d5cba7324575140b47500
vsee <volker@seekerscience.co.uk>
2017-03-03 15:12:29 +0000

    Energy and performance charts added
    Energy and performance charts are displayed in the gui using the xcharts java library.
    For each execution run total energy and runtime are claculated from the returned results
    and added as new data point to the displayed graphs.
    
    TODO: evaluation and display of error cases. Right now they are displayed in the console.
    TODO: evaluation of different possible mappings.

--- src/main/java/lancs/dividend/oclBenchMapper/ui/ClientGui.java
+++ src/main/java/lancs/dividend/oclBenchMapper/ui/ClientGui.java
@@ -7,7 +7,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
@@ -20,29 +22,72 @@ import javax.swing.border.TitledBorder;
 import lancs.dividend.oclBenchMapper.client.ClientConnectionHandler;
 import lancs.dividend.oclBenchMapper.connection.ServerConnection;
 import lancs.dividend.oclBenchMapper.mapping.ExecutionItem;
+import lancs.dividend.oclBenchMapper.message.response.BenchStatsResponseMessage;
+import lancs.dividend.oclBenchMapper.message.response.ErrorResponseMessage;
+import lancs.dividend.oclBenchMapper.message.response.ResponseMessage;
+import lancs.dividend.oclBenchMapper.message.response.TextResponseMessage;
 import lancs.dividend.oclBenchMapper.server.RodiniaRunner.DataSetSize;
 import lancs.dividend.oclBenchMapper.server.RodiniaRunner.RodiniaBin;
-import lancs.dividend.oclBenchMapper.userCmd.ExitCmd;
+import lancs.dividend.oclBenchMapper.userCmd.RunBenchCmd;
 import lancs.dividend.oclBenchMapper.userCmd.UserCommand;
 import lancs.dividend.oclBenchMapper.userCmd.UserCommand.CmdType;
+
+import org.knowm.xchart.XChartPanel;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries.XYSeriesRenderStyle;
+import org.knowm.xchart.style.Styler.LegendPosition;
 
 public class ClientGui implements UserInterface {
 
 	// TODO clean up initialise
+	// TODO add graceful shutdown upon window close
 	
 	private JFrame frame;
 	private JComboBox<RodiniaBin> workloadcbox;
 	private JComboBox<DataSetSize> datacbox;
+	private XChartPanel<XYChart> eChartPanel;
+	private XChartPanel<XYChart> pChartPanel;
+	
+	private XYChart energyChart;
+	private XYChart performanceChart;
 	
 	private ClientConnectionHandler cmdHandler;
 	
+	private double iteration = 0;
+	private final List<Double> energyData = new ArrayList<>();
+	private final List<Double> performanceData = new ArrayList<>();
+	private final List<Double> iterationData = new ArrayList<>();
+	
 	public ClientGui() {
-		initialise();
+		initialiseGraphs();
+		initialiseGui();
 	}
 	
-	private void initialise() {
+	private void initialiseGraphs() {
+		energyChart = new XYChartBuilder().width(600).height(400).title("Energy Consumption")
+				.xAxisTitle("Iteration").yAxisTitle("Energy in Joules").build();
+		energyChart.getStyler().setLegendPosition(LegendPosition.InsideNE);
+		energyChart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Line);
+		energyChart.getStyler().setXAxisMin(0.0);
+		
+		performanceChart = new XYChartBuilder().width(600).height(400).title("Performance")
+				.xAxisTitle("Iteration").yAxisTitle("Execution Time in ms").build();
+		performanceChart.getStyler().setLegendPosition(LegendPosition.InsideNE);
+		performanceChart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Line);
+		performanceChart.getStyler().setXAxisMin(0.0);
+		
+		energyData.add(0.0);
+		performanceData.add(0.0);
+		iterationData.add(iteration++);
+		
+		energyChart.addSeries("mapper", iterationData, energyData);
+		performanceChart.addSeries("mapper", iterationData, performanceData);
+	}
+
+	private void initialiseGui() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 1200, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.addWindowListener(new WindowAdapter() {
 	        public void windowClosing(WindowEvent winEvt) {
@@ -108,6 +153,26 @@ public class ClientGui implements UserInterface {
 		gbc_wlpanel.gridx = 0;
 		gbc_wlpanel.gridy = 1;
 		frame.getContentPane().add(wlPanel, gbc_wlpanel);
+		GridBagLayout gbl_wlPanel = new GridBagLayout();
+		gbl_wlPanel.columnWidths = new int[]{0, 0, 0};
+		gbl_wlPanel.rowHeights = new int[]{0, 0};
+		gbl_wlPanel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_wlPanel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		wlPanel.setLayout(gbl_wlPanel);	    
+		
+		eChartPanel = new XChartPanel<XYChart>(energyChart);
+		GridBagConstraints gbc_echart = new GridBagConstraints();
+		gbc_echart.fill = GridBagConstraints.BOTH;
+		gbc_echart.gridx = 0;
+		gbc_echart.gridy = 0;
+		wlPanel.add(eChartPanel, gbc_echart);
+		
+		pChartPanel = new XChartPanel<XYChart>(performanceChart);
+		GridBagConstraints gbc_pchart = new GridBagConstraints();
+		gbc_pchart.fill = GridBagConstraints.BOTH;
+		gbc_pchart.gridx = 1;
+		gbc_pchart.gridy = 0;
+		wlPanel.add(pChartPanel, gbc_pchart);
 	}
 	
 	@Override
@@ -130,16 +195,81 @@ public class ClientGui implements UserInterface {
 	
 	@Override
 	public UserCommand receiveCommand() {
-		// TODO add command handling
-		
-		return new ExitCmd();
+		RodiniaBin rbin = (RodiniaBin) workloadcbox.getSelectedItem();
+		DataSetSize dsetSize = (DataSetSize) datacbox.getSelectedItem();
+		return new RunBenchCmd(rbin, dsetSize);
 	}
-
+	
 	@Override
 	public void updateDisplay(
 			Hashtable<ServerConnection, ExecutionItem> executionMap, UserCommand cmd) {
+		if(executionMap == null || executionMap.size() == 0)
+			throw new RuntimeException("Given execution map must not be null or empty.");
+		if(cmd == null)
+			throw new RuntimeException("Given user command must not be null.");
 		
-		// TODO add graph update
+		boolean validResponse = true;
+		double totalEnergyJ = 0;
+		double totalRuntimeMS = 0;
+		for (ServerConnection s : executionMap.keySet()) {
+			ExecutionItem item = executionMap.get(s);
+
+			System.out.println("\nExecution result of server " + s);
+			System.out.println("Command:\n\t" + item.getCommand());
+			System.out.println("Response:");
+			
+			if(!item.resultsAvailable()) {
+				System.out.println("ERROR: No results received!");
+				validResponse = false;
+			} else {
+				ResponseMessage response = item.getResponse();
+				
+				switch (response.getType()) {
+				case TEXT:
+					System.out.println("\t" + ((TextResponseMessage) response).getText());
+					validResponse = false;
+					break;
+				case BENCHSTATS:
+					BenchStatsResponseMessage br = (BenchStatsResponseMessage) response;
+					System.out.println("### Execution standard output:\n" + br.getStdOut());
+					System.out.println("### Has Energy Log: " + br.hasEnergyLog());
+					
+					if(br.hasEnergyLog()) {
+						System.out.println("### Energy Log:");
+						System.out.println(br.getEnergyLog().getLogRecords().size() + " log entries found.");
+						System.out.println("### Energy: " + br.getEnergyLog().getEnergyJ() + " J");
+						System.out.println("### Runtime: " + br.getEnergyLog().getRuntimeMS() + " ms");
+					}
+					
+					totalEnergyJ += br.getEnergyLog().getEnergyJ();
+					totalRuntimeMS += br.getEnergyLog().getRuntimeMS();
+					break;
+				case ERROR:
+					System.err.println("\tERROR: " + ((ErrorResponseMessage) response).getText());
+					validResponse = false;
+					break;
+				default:
+					System.err.println("\tUnknown response type: " + response.getType());
+					validResponse = false;
+					break;
+				}
+			}
+		}
+		
+		if(validResponse) updateGraphs(totalEnergyJ, totalRuntimeMS);
+	}
+	
+	private void updateGraphs(double energyJ, double runtimeMS) {
+		iterationData.add(iteration++);
+		energyData.add(energyJ);
+		performanceData.add(runtimeMS);
+		
+		energyChart.updateXYSeries("mapper", iterationData, energyData, null);
+		performanceChart.updateXYSeries("mapper", iterationData, performanceData, null);
+		eChartPanel.revalidate();
+		eChartPanel.repaint();
+		pChartPanel.revalidate();
+		pChartPanel.repaint();
 	}
 
 }
